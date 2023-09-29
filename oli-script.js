ceffb92adf8ba4d49fe5036c6fb014783a07a1bb
olinarr <olivieronardi@protonmail.com>
2023-09-29 11:04:27 +0200

    javascript!

---
+++ oli-script.js
@@ -0,0 +1,8 @@
+function togglePicture() {
+    if (document.getElementById("oliviero-picture").style.display == 'inline') {
+        document.getElementById("oliviero-picture").style.display = 'none';
+    }
+    else {
+        document.getElementById("oliviero-picture").style.display = 'inline';   
+    }
+}
