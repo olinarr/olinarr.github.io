6a77c47f3945ca7f6a404c8ddc4f392a98ac485e
olinarr <olivieronardi@protonmail.com>
2023-12-11 12:08:56 +0100

    Picture button now also toggles picture creditsn

--- oli-script.js
+++ oli-script.js
@@ -1,8 +1,11 @@
 function togglePicture() {
     if (document.getElementById("oliviero-picture").style.display == 'inline') {
         document.getElementById("oliviero-picture").style.display = 'none';
+        document.getElementById("picture-credits-text").style.display = 'none';
+        
     }
     else {
-        document.getElementById("oliviero-picture").style.display = 'inline';   
+        document.getElementById("oliviero-picture").style.display = 'inline';
+        document.getElementById("picture-credits-text").style.display = 'inline';
     }
 }
